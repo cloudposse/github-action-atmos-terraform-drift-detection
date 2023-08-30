1e46af377f74aff5e50bb162d6f1edad2bf14e3c
Zinovii Dmytriv <zdmytriv@gmail.com>
2023-08-30 17:40:32 +0300

    update

--- src/action.js
+++ src/action.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const core = require('@actions/core');
 const {
     downloadArtifacts,
@@ -47,13 +48,94 @@ const mapOpenGitHubIssuesToComponents = async (octokit, context) => {
       }
     }
 
-    core.info(`components-to-issues: ${JSON.stringify(componentsToIssues, null, 4)}`);
-    core.info(`components-to-metadata: ${JSON.stringify(componentsToMetadata, null, 4)}`);
-
     return {
         componentsToIssues: componentsToIssues,
         componentsToMetadata: componentsToMetadata,
     };
+}
+
+const readMetadataFromPlanArtifacts = async () => {
+    const files = fs.readdirSync('.');
+    const metadataFiles = files.filter(file => file.endsWith('metadata.json'));
+
+    let componentsToState = {};
+    let componentsToMetadata = {};
+
+    for (let i = 0; i < metadataFiles.length; i++) {
+        const metadata = JSON.parse(fs.readFileSync(metadataFiles[i], 'utf8'));
+
+        const slug = `${metadata.stack}-${metadata.component}`;
+        const drifted = metadata.drifted;
+
+        componentsToState[slug] = drifted;
+        componentsToMetadata[slug] = metadata;
+    }
+
+    return {
+        componentsToState: componentsToState,
+        componentsToMetadata: componentsToMetadata,
+    };
+}
+
+const triage = async(componentsToIssueNumber, componentsToIssueMetadata, componentsToPlanState) => {
+    let slugs = new Set([...Object.keys(componentsToIssueNumber), ...Object.keys(componentsToPlanState)]);
+
+    const componentsCandidatesToCreateIssue = [];
+    const componentsCandidatesToCloseIssue = [];
+    const componentsToUpdateExistingIssue = [];
+    const removedComponents = [];
+    const recoveredComponents = [];
+    const driftingComponents = [];
+
+    for (let slug of slugs) {
+        if (componentsToIssueNumber.hasOwnProperty(slug)) {
+            const issueNumber = componentsToIssueNumber[slug];
+
+            if (componentsToPlanState.hasOwnProperty(slug)) {
+                const drifted = componentsToPlanState[slug];
+
+                if (drifted) {
+                const commitSHA = componentsToIssueMetadata[slug].commitSHA;
+                const currentSHA = "${{ github.sha }}";
+                if (currentSHA === commitSHA) {
+                    core.info(`Component "${slug}" marked as drifted but default branch SHA didn't change so nothing to update. Skipping ...`);
+                    driftingComponents.push(slug);
+                } else {
+                    core.info(`Component "${slug}" is still drifting. Issue ${issueNumber} needs to be updated.`);
+                    componentsToUpdateExistingIssue.push(slug);
+                    driftingComponents.push(slug);
+                }
+                } else {
+                core.info(`Component "${slug}" is not drifting anymore. Issue ${issueNumber} needs to be closed.`);
+                componentsCandidatesToCloseIssue.push(slug);
+                recoveredComponents.push(slug);
+                }
+            } else {
+                core.info(`Component "${slug}" has been removed. Issue ${issueNumber} needs to be closed.`);
+                componentsCandidatesToCloseIssue.push(slug);
+                removedComponents.push(slug);
+            }
+        } else {
+            const drifted = componentsToPlanState[slug];
+
+            if (drifted) {
+                core.info(`Component "${slug}" drifted. New issue has to be created.`);
+                componentsCandidatesToCreateIssue.push(slug);
+                driftingComponents.push(slug);
+            } else {
+                core.info(`Component "${slug}" is not drifting. Skipping ...`);
+            }
+        }
+    }
+
+    return {
+        componentsCandidatesToCreateIssue: componentsCandidatesToCreateIssue,
+        componentsToUpdateExistingIssue: componentsToUpdateExistingIssue,
+        removedComponents: removedComponents,
+        recoveredComponents: recoveredComponents,
+        driftingComponents: driftingComponents,
+        componentsCandidatesToCloseIssue: componentsCandidatesToCloseIssue,
+    }
 }
 
 /**
@@ -71,11 +153,22 @@ const runAction = async (octokit, context, parameters) => {
     downloadArtifacts("metadata");
 
     const openGitHubIssuesToComponents = await mapOpenGitHubIssuesToComponents(octokit, context);
-    const componentsToIssues = openGitHubIssuesToComponents.componentsToIssues;
-    const componentsToMetadata = openGitHubIssuesToComponents.componentsToMetadata;
+    const componentsToIssueNumber = openGitHubIssuesToComponents.componentsToIssues;
+    const componentsToIssueMetadata = openGitHubIssuesToComponents.componentsToMetadata;
 
-    core.info(`components-to-issues: ${JSON.stringify(componentsToIssues, null, 4)}`);
-    core.info(`components-to-metadata: ${JSON.stringify(componentsToMetadata, null, 4)}`);
+    const metadataFromPlanArtifacts = await readMetadataFromPlanArtifacts();
+    const componentsToPlanState = metadataFromPlanArtifacts.componentsToState;
+    const componentsToPlanMetadata = metadataFromPlanArtifacts.componentsToMetadata;
+
+    const triageResults = await triage(componentsToIssueNumber, componentsToIssueMetadata, componentsToPlanState);
+    const componentsCandidatesToCreateIssue = triageResults.componentsCandidatesToCreateIssue;
+    const componentsToUpdateExistingIssue = triageResults.componentsToUpdateExistingIssue;
+    const removedComponents = triageResults.removedComponents;
+    const recoveredComponents = triageResults.recoveredComponents;
+    const driftingComponents = triageResults.driftingComponents;
+    const componentsCandidatesToCloseIssue = triageResults.componentsCandidatesToCloseIssue;
+
+    core.info(`Components candidates to create issue: ${componentsCandidatesToCreateIssue}`);
 };
 
 module.exports = {
