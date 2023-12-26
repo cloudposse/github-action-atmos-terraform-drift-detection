1511e5b88f68b89591e89e2c040551143adbdcc6
Igor Rodionov <goruha@gmail.com>
2023-12-26 21:42:26 +0100

    Refactor GHA

--- src/action.js
+++ src/action.js
@@ -65,9 +65,13 @@ const mapOpenGitHubIssuesToComponents = async (octokit, context) => {
 
     let per_page = 100; // Max allowed value per page
 
-    return new Map(listIssues(per_page, 1, []).map( (stackFromIssue) => {
-        return [stackFromIssue.slug, stackFromIssue]
-    }))
+    return new Map(listIssues(per_page, 1, []).finally(
+        (result) => {
+            return result.map( (stackFromIssue) => {
+                return [stackFromIssue.slug, stackFromIssue]
+            }
+        }
+    )))
 }
 
 // const readMetadataFromPlanArtifacts = async () => {
