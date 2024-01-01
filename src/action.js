0f2d37dde3dc1e69823ace179378d826de2ecc8d
Igor Rodionov <goruha@gmail.com>
2024-01-01 22:27:58 +0100

    Fix failed stack

--- src/action.js
+++ src/action.js
@@ -109,7 +109,7 @@ const triage = async (componentsToIssue, componentsToPlanState, users, labels, m
     })
 
     const openedIssuesCounts = operations.filter((operation) => {
-        return operation instanceof Create || operation instanceof Update
+        return operation instanceof Update
     }).length
 
     const closedIssuesCount = operations.filter((operation) => {
