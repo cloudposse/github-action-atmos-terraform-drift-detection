6428dc18b9726650c70e4e70704003e8c2e2ac68
Igor Rodionov <goruha@gmail.com>
2024-01-16 23:57:20 +0100

    Change table in PR mode

--- src/results/none.js
+++ src/results/none.js
@@ -1,9 +1,5 @@
 class None {
-    constructor(context, repository, newIssue, state) {
-        this.runId = context.runId;
-        this.repository = repository;
-        this.newIssue = newIssue;
-        this.state = state;
+    constructor() {
     }
 
     render() {
