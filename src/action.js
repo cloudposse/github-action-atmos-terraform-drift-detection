86306392d6642b52ccd545152c4b26c5d7b2d058
Igor Rodionov <goruha@gmail.com>
2024-01-16 21:56:43 +0100

    Update PR comments

--- src/action.js
+++ src/action.js
@@ -227,9 +227,7 @@ const postComment = async (octokit, context, table) => {
       ...context.repo,
       issue_number: context.payload.pull_request.number,
     }).then( result => {
-      return result.data.filter(item => {
-        return item.body.includes(commentIdSuffix)
-      })
+      return result.data
     })
     // }).then( result => {
     //   return result.pop()
