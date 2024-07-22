2e02161ddd405eb61d29e7d67fa1a6178fc9f22d
milldr <miller0daniel@gmail.com>
2024-07-22 13:48:43 -0400

    Fix artifactClient instantiation in downloadArtifacts function

--- src/action.js
+++ src/action.js
@@ -14,7 +14,7 @@ const {readFileSync} = require("fs");
 
 
 const downloadArtifacts = (artifactName) => {
-  const artifactClient = artifact.create()
+  const artifactClient = new artifact()
   const downloadDirectory = '.'
 
   // Downloading the artifact
